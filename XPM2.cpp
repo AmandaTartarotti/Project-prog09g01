3011c69609bb654d3e9453e549c8e40a9804e343
Rdest026 <rafaelsbo1@hotmail.com>
2023-05-19 11:06:17 -0300

    Attempt to fix an error in xpm2_open

--- XPM2.cpp
+++ XPM2.cpp
@@ -11,25 +11,25 @@ namespace prog {
             return nullptr;
         }
 
-        int widthImg, heightImg, colorImg;
-        inputFile >> widthImg >> heightImg >> colorImg;
+        int widthImg, heightImg, countColorImg;
+        inputFile >> widthImg >> heightImg >> countColorImg;
 
-        std::vector<std::string> colorPalette(colorImg);
-        for (int i = 0; i < colorImg; ++i) {
+        std::vector<std::string> colorPalette(countColorImg);
+        for (int i = 0; i < countColorImg; i++) {
             inputFile >> colorPalette[i];
         }
 
         std::vector<std::vector<std::string>> pixels(heightImg, std::vector<std::string>(widthImg));
-        for (int y = 0; y < heightImg; ++y) {
+        for (int y = 0; y < heightImg; y++) {
             std::string pixelRow;
             inputFile >> pixelRow;
             if (pixelRow.length() != widthImg) {
                 //Invalid XPM data: insufficient pixels in some row
                 return nullptr;
             }
-            for (int x = 0; x < widthImg; ++x) {
+            for (int x = 0; x < widthImg; x++) {
                 char colorIndex = pixelRow[x];
-                if (colorIndex >= '0' && colorIndex < '0' + colorImg) {
+                if (colorIndex >= '0' && colorIndex < '0' + countColorImg) {
                     pixels[y][x] = colorPalette[colorIndex - '0'];
                 } else {
                     //Invalid XPM data: invalid color index at some position
